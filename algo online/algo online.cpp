830fccf72a8b526a0d1c024f5760f6aa1046a63f
Putranugroho14 <putranugroho61@gmail.com>
2024-05-11 12:01:22 +0700

    memanggil semua perintah

--- algo online/algo online.cpp
+++ algo online/algo online.cpp
@@ -58,10 +58,54 @@ public:
 
 	};
 
-	
-int main()
-{
-   
-}
+	int main() {
+		Stack stack;
 
+		int choice = 0;
+		int value;
+
+		while (choice != 5) {
+			cout << "1. Push\n";
+			cout << "2. Pop\n";
+			cout << "3. Peek\n";
+			cout << "4. Exit\n";
+			cout << "Enter your choice: ";
+			cin >> choice;
+
+			switch (choice) {
+			case 1:
+				cout << "Enter the value to push: ";
+				cin >> value;
+				stack.push(value);
+				break;
+			case 2:
+				if (!stack.isEmpty()) {
+					stack.pop();
+				}
+				else {
+					cout << "stack is empty. Cannot pop." << endl;
+				}
+				break;
+			case 3:
+				if (!stack.isEmpty()) {
+					stack.peek();
+				}
+				else {
+					cout << "Stack is empty. No top value." << endl;
+				}
+				break;
+
+			case 4:
+				cout << "Exiting program." << endl;
+				break;
+			default:
+				cout << "Invalid choice. Try again." << endl;
+				break;
+			}
+
+			cout << endl;
+		}
+
+		return 0;
+	}
 
